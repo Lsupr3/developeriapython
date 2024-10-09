2db91c6daf2fde5093dfe53d3a2d372a1545ee6e
Juan Quijano <listas@juanquijano.es>
2024-10-09 13:53:00 +0200

    Calculo d parsecs

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hola Mundo");
+from datetime import date
+
+print("Hoy es: " + str(date.today()))
+
+parsecs = 11
+lightyears = parsecs * 3.26
+print('{0} parsecs is {1} lightyears.'.format(parsecs, lightyears))
