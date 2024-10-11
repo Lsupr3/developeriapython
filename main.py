d097babc4063fefb106d0f026bd346a873cc27d2
Juan Quijano <listas@juanquijano.es>
2024-10-11 11:53:25 +0200

    Hallar la media de valores

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 import os
-os.system('cls')
+import numpy as np
+# pip import numpy
 
-rainfall = {
-    'october': 3.5,
-    'november': 4.2,
-    'december': 2.1
+os.system('cls')
+planet_moons = {
+    'mercury': 0,
+    'venus': 0,
+    'earth': 1,
+    'mars': 2,
+    'jupiter': 79,
+    'saturn': 82,
+    'uranus': 27,
+    'neptune': 14,
+    'pluto': 5,
+    'haumea': 2,
+    'makemake': 1,
+    'eris': 1
 }
 
-'''for item in rainfall.items():
-    print(f'{item[0]}: {item[1]}cm')
-
-for key in rainfall.keys():
-    print(f'{key}: {rainfall[key]}cm')
-'''
-
-if 'december' in rainfall:
-    rainfall['december'] += 1
-else:
-    rainfall['december'] = 1
-
-for key, value in rainfall.items():
-    print(f'{key}: {value}cm')
-
-total_rainfall = 0
-for value in rainfall.values():
-    total_rainfall = total_rainfall + value
-
-print(f'There was {total_rainfall}cm in the last quarter.')
-print(f'There was {sum(rainfall.values())}cm in the last quarter.')
+print(planet_moons.keys())
+print(len(planet_moons))
+print(np.mean(list(planet_moons.values())))
