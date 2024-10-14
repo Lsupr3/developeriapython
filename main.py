6a7c283a256fa50866a7e64a71f5e36868b9c9a1
Juan Quijano <listas@juanquijano.es>
2024-10-14 11:42:14 +0200

    Control de errores con try

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-import os
-os.system("cls")
+def main():
+    try:
+        configuration = open('config.txt')
 
-def genera_reporte(**fueltank):
-    for name, value in fueltank.items():
-        print (f"{name}: {value}")
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except PermissionError as e:
+        print(e)
+    except Exception as e:
+        print(type(e))
+    finally:
+        print("Todo correcto")
 
-genera_reporte(main=50, external=80, hidrogen=40)
+main()
 
-def funcion(*parametro):
-    print()
-
-funcion()
+try:
+    open("config.txt")
+except OSError as err:
+     if err.errno == 2:
+         print("Couldn't find the config.txt file!")
+     elif err.errno == 13:
+        print("Found config.txt but couldn't read it")
