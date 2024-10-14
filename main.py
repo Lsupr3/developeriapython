41bcaf41686c0e6f52e6ad72b9312f259a38c97c
Juan Quijano <listas@juanquijano.es>
2024-10-14 12:51:00 +0200

    Crear errores

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-loaded_config = """# Rocket Ship Configuration File!
-fuel_tanks=4
-oxygen_tanks=3
-initial_propulsion_level=84
-$ End of file"""
+def alert_navigation_system(err):
+    print(err)
 
-parsed_config = {}
-for line in loaded_config.split("\n"):
-    try:
-        key, value = line.split("=")
-        parsed_config[key] = value
-    except ValueError:
-        print("Valor no valido")
-        
-print(parsed_config)
+def water_left(astronauts, water_left, days_left):
+
+    for argument in [astronauts, water_left, days_left]:
+        try:
+            argument / 10
+        except TypeError:
+            raise RuntimeError(f"All arguments must be of type int, but received: '{argument}'")
+  
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+
+try:
+    water_left("5", "100", 2)
+except RuntimeError as err:
+    alert_navigation_system(err)
+
