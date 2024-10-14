a973105a0fc6c939b8a3b123655e8079163fb94a
Juan Quijano <jc_quijano@hotmail.com>
2024-10-14 21:02:06 +0200

    Variable privada. Getter y Setter con decoradores

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-true_values = ['yes', 'y']
-false_values = ['no', 'n']
+import os
+os.system('cls')
 
 
-def str2bool(value):
-    value = value.lower()
-    if  value in true_values:
-        return True
-    if value in false_values:
-        return False
-    raise ValueError("Tipo erroneo")
+class Square:
+    def __init__(self, ancho, alto):
+        self.__height = ancho
+        self.__width = alto
 
-try:
-    print(str2bool("1"))
-except Exception as ex:
-    print(Exception)
+    def set_side(self, new_side):
+        self.__height = new_side
+        self.__width = new_side
+
+    def isSquare(self):
+        if self.__height == self.__width:
+            return True
+        else:
+            return False
     
+    @property    
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, new_value):
+        if new_value >= 0:
+            self.__height = new_value
+        else:
+            raise Exception("value needs to be 0 or larger")
+
+square = Square(2, 2)
+square.height = 2
+square.set_side(3)
+print(square.isSquare())
+print(square.height)
