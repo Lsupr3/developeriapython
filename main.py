9d5be6c21a3303be5f8e93d406a802e592e50067
Juan Quijano <listas@juanquijano.es>
2024-10-14 12:31:49 +0200

    control de excepciones

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-def main():
+loaded_config = """# Rocket Ship Configuration File!
+fuel_tanks=4
+oxygen_tanks=3
+initial_propulsion_level=84
+$ End of file"""
+
+parsed_config = {}
+for line in loaded_config.split("\n"):
     try:
-        configuration = open('config.txt')
-
-    except FileNotFoundError:
-        print("Couldn't find the config.txt file!")
-    except IsADirectoryError:
-        print("Found config.txt but it is a directory, couldn't read it")
-    except PermissionError as e:
-        print(e)
-    except Exception as e:
-        print(type(e))
-    finally:
-        print("Todo correcto")
-
-main()
-
-try:
-    open("config.txt")
-except OSError as err:
-     if err.errno == 2:
-         print("Couldn't find the config.txt file!")
-     elif err.errno == 13:
-        print("Found config.txt but couldn't read it")
+        key, value = line.split("=")
+        parsed_config[key] = value
+    except ValueError:
+        print("Valor no valido")
+        
+print(parsed_config)
