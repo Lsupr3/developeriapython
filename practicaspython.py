d64b73e7584dfb5e6a1b04e73f773df7c1ba32df
Juan Quijano <listas@juanquijano.es>
2025-01-07 18:47:14 +0000

    Calculadora

--- practicaspython.py
+++ practicaspython.py
@@ -2,6 +2,9 @@ import os
 os.system('clear')
 import sys
 
-print(sys.argv)
-print(sys.argv[0]) #program name
-print(sys.argv[1]) #argumento uno
+#print("Bienvenido al programa que saluda")
+#name = input("Escribe tu nombre: ")
+#print("Hola " + name)
+numero1 = input("Primer número: ")
+numero2 = input("Segundo número: ")
+print(int(numero1) + int(numero2))
