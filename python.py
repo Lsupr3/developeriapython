5950f13dcf00eefc1ae47519cd580c05b5bf2bd8
Juan Quijano <listas@juanquijano.es>
2025-01-07 18:16:59 +0000

    Empezando nuevo curso básico de Python. Print, variables y f-string

---
+++ python.py
@@ -0,0 +1,19 @@
+import os
+os.system('clear')
+
+
+mensajesuma = "El resultado de la suma es: "
+suma = 1 + 2 #3
+mensajeproducto = "El resultado de la multiplicación"
+producto = suma * 2
+print("El resultado de la suma es " + str(suma))
+print("El resultado de la multiplicación es: " + str(producto))
+
+name = "Juan"
+age = 30
+print(f"Hola, {name}. Tienes {age} años")
+
+pi = 3.1415902
+print(f"El valor de pi es: {pi:.4f}")
+print(f"Pi es del tipo {type(pi)}")
+print("Pi es del tipo: " + str(type(pi)))
